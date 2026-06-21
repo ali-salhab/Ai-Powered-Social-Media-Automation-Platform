808f56df718432fe1c0f76adb00897d1b84eaf93
ali-salhab <alysalhab5@gmail.com>
2026-06-21 10:25:04 +0300

    Refactor Layout component background and add padding to Dashboard page

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ import React from "react";
  * Renders the dashboard page.
  */
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return <div className="p-4 ">Dashboard</div>;
 }
 
 export default Dashboard;
